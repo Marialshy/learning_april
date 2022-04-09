7870c1e120a561be512f063a871f96e3e73935b0
Maria <marialshy@gmail.com>
2022-04-09 23:56:08 +0300

    example 6 - compare max

---
+++ example006_compareMax/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 13;
+int c = 8;
+int d = 5;
+int e = 7;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
