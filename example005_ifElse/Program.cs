68c89317ef8bda3559af665fe54a055cff3cea28
Maria <marialshy@gmail.com>
2022-04-09 23:38:11 +0300

    example 5 -if/else

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите имя:");
+string username = Console.ReadLine();
+if (username =="Мари")
+{
+    Console.WriteLine("Мари, хорошего дня ^^~");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.Write(username);
+}
