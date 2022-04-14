14c02d3e9c4dc004cf96797a2ebbc799cdd41b46
Maria <marialshy@gmail.com>
2022-04-14 11:07:36 +0300

    add seminars 1,2

---
+++ seminar2_11/Program.cs
@@ -0,0 +1,7 @@
+﻿// для случайного числа в диапазоне 100-999 вывести его наибольшую цифру
+int n = new Random().Next(100,1000);
+int digit1 = n %10;
+int digit3 = n /100;
+int result = digit3*10 + digit1;
+
+Console.WriteLine("Для числа "+ n +" двузначное число "+ result +"");
---
+++ seminar2_9/Program.cs
@@ -0,0 +1,13 @@
+﻿// для случайного числа в диапазоне 10-99 вывести его наибольшую цифру
+int n = new Random().Next(10,100);
+int digit1 = n %10;
+int digit2 = n /10;
+
+if (digit1 > digit2)
+{
+    Console.WriteLine("Для числа "+ n +" наибольшая цифра "+ digit1 +"");
+}
+else
+{
+Console.WriteLine("Для числа " + n +" наибольшая цифра = "+ digit2 +"");
+}
