9c910ce919fbbf59ec78236ca848e2a9eee0b2dd
Maria <marialshy@gmail.com>
2022-04-14 12:05:37 +0300

    add example for multiplicity, main branch test

---
+++ seminar2_12_multiplicity/Program.cs
@@ -0,0 +1,16 @@
+﻿// ввести 2 числа, если первое не кратно второму, то вывести остаток деления
+Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1%number2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно, остаток деления = " + number1%number2 + "");
+}
+//Convert.ToInt32 - перевод в целое число;
+
