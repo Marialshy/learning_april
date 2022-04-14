175e078d9282658da2017b992fb528f721e41fbe
Maria <marialshy@gmail.com>
2022-04-14 11:59:32 +0300

    add example for multiplicity, 2 branches

---
+++ seminar2_12_multiplicity/Program.cs
@@ -0,0 +1,20 @@
+﻿// ввести 2 числа, если первое не кратно второму, то вывести остаток деления
+Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+double result1 = number1/number2;
+double result2 = result1%10;
+double result3 = number1 - number2*result2;
+
+if (number1%number2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("не кратно, остаток деления = " + result3 + "");
+}
+//Convert.ToInt32 - перевод в целое число;
+
