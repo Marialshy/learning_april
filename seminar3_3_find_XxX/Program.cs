44338f31909ba5e7183d27ddf8f690218b3bb352
Maria <marialshy@gmail.com>
2022-04-19 20:27:09 +0300

    seminar 3 - find x*x till n

---
+++ seminar3_3_find_XxX/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter number");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+getsq (n);
+void getsq (int n)
+{
+    while (count<=n)
+    {
+        int newn = count;
+        count = count*count;
+        Console.Write(""+ count +", ");
+        count = newn + 1;
+    }
+}
