b0910068f4e074a98b7cca705f2a2d4f29ac2a18
Maria <marialshy@gmail.com>
2022-04-26 14:16:17 +0300

    seminar 4 - other group

---
+++ seminar4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// сумма числе от 1 до n
+
+int naturalSum (int a)
+{
+    int sum = 0;
+    if (a<1) return 0;
+    else
+    {
+        for(int i =1; i<=a; i++)
+        {
+            sum += i; // sum = sum +i то же что и sum+=i
+        }
+        return sum;
+    }
+}
+
+Console.Write("enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("sum: " + naturalSum(num));
---
+++ seminar4_2/Program.cs
@@ -0,0 +1,9 @@
+﻿//вывести на экран массив из 8 элементов с рандомынми числами 0 или 1
+int []array= new int [8];
+for (int i = 0; i< array.Length; i++)
+{
+    array[i]=new Random().Next(0,2);
+    Console.Write(array [i]+ ",");
+}
+//Console.Write(PrintArray(array)) - написав при помощи метода PrintArray;
+
